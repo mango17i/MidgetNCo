b83e37f5318e5bc430335bc16e4ac625b799d852
mika brun <brunmikael.mars@gmail.com>
2021-08-17 11:13:11 +0200

    Added code to offer & reservation controller + seeds

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,23 @@
 class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @offer = Offer.find(params[:offer_id])
+    @reservation.offer = @offer
+
+    if @reservation.save
+      redirect_to offer_path(params[:offer_id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
 end
