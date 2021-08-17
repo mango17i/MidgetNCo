212a746c7b87d0e758d50d2d0e0b5022a72a0a88
sacha bojanowski <bojanowski.sacha@gmail.com>
2021-08-17 12:18:59 +0200

    Changed validations again walah

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,8 +2,8 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :reservations
 
-  validates :title, length: { minimum: 8, maximum: 20 }
-  validates :description, length: { maximum: 140 }
+  # validates :title, length: { minimum: 8, maximum: 20 }
+  # validates :description, length: { maximum: 140 }
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true
