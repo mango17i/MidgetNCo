7cab6c0d8672800f928fc92d0766dea443a035a1
mika brun <brunmikael.mars@gmail.com>
2021-08-18 15:53:40 +0200

    added userpage de ses morts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+    @offers = @user.offers
+    @reservations = @user.reservations
+  end
+
   def user_params
     params.require(:user).permit(:name, :description, :photo)
   end
