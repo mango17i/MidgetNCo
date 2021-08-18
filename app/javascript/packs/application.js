76c0d2a2bf31334c1e922b9b505766ac2e773164
mika brun <brunmikael.mars@gmail.com>
2021-08-18 14:35:55 +0200

    Flatpickr fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,13 +27,14 @@ import "bootstrap";
 import { initUpdateNavbarOnScroll } from './navbar';
 import { initFlatpickr } from "../plugins/flatpickr";
 
-initFlatpickr();
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFlatpickr();
 });
 
 document.addEventListener('turbolinks:load', () => {
