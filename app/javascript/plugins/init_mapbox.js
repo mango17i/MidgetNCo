524966e163c7ee898fe783a3496e5a8e938ea664
sacha bojanowski <bojanowski.sacha@gmail.com>
2021-08-19 12:30:46 +0200

    info window on markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -22,6 +22,15 @@ const initMapbox = () => {
         .addTo(map);
     });
     fitMapToMarkers(map, markers);
+    const addMarkersToMap = (map, markers) => {
+      markers.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+        new mapboxgl.Marker()
+          .setLngLat([marker.lng, marker.lat])
+          .setPopup(popup) // add this
+          .addTo(map);
+      });
+    };
   }
 };
 
