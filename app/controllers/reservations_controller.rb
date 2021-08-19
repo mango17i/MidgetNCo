83680bd2b6d4f8740add702a42570b40e62e2400
mika brun <brunmikael.mars@gmail.com>
2021-08-19 15:58:57 +0200

    Reservation edit and delete working

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,10 +11,31 @@ class ReservationsController < ApplicationController
     @reservation.user_id = current_user.id
 
     if @reservation.save!
-      redirect_to offer_path(params[:offer_id])
+      redirect_to user_path(current_user.id)
     else
       render :new
     end
+  end
+
+  def edit
+    @offer = Offer.find(params[:offer_id])
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+    @reservation.specs = params['reservation']['specs']
+    @reservation.start_date = params['reservation']['start_date']
+    @reservation.end_date = params['reservation']['end_date']
+    @reservation.save!
+    redirect_to user_path(current_user.id)
+  end
+
+  def destroy
+    @offer = Offer.find(params[:offer_id])
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to user_path(current_user.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :offers do
-    resources :reservations, only: %i[new create]
+    resources :reservations, only: %i[new create update edit destroy]
   end
   resources :users, only: [:index, :show].freeze
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
